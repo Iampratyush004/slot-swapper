d1c5e09391a1fd88f9a6f18f4920604a767857b1
Pratyush Agrawal <149037331+Iampratyush004@users.noreply.github.com>
2025-10-31 15:10:08 +0530

    added in server ts app get

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,9 @@
 import { app } from "./app";
 
 const port = process.env.PORT ? Number(process.env.PORT) : 4000;
+app.get("/", (req, res) => {
+  res.send("SlotSwapper API is running 🚀");
+});
 
 app.listen(port, () => {
   console.log(`SlotSwapper backend listening on http://localhost:${port}`);
